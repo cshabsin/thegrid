6e757c93e246c11b511bad65ac2de955ad59f2a4
Chris Shabsin <cshabsin@gmail.com>
2022-05-14 21:33:51 -0400

    Add arrow definitions from original

--- example/example.go
+++ example/example.go
@@ -9,14 +9,12 @@ import (
 
 func main() {
 	document := js.Document()
-	// p := document.CreateElement("p")
-	// p.Set("innerHTML", "hi")
-	// document.Body().Append(p)
 	hm := hexmap.NewHexMap(10, 11, 70, false)
 	mapGroup := document.CreateSVG("g", js.MakeAttr("class", "map-anchor-group"), js.MakeAttr("transform", "translate(10,10)"))
 	mapGroup.Append(hm.GridMesh().MakeSVG(document, js.Class("map-mesh")))
 
-	svg := document.CreateSVG("svg", js.MakeAttr("height", fmt.Sprintf("%fpx", hm.GetPixHeight()+20)), js.MakeAttr("width", fmt.Sprintf("%fpx", hm.GetPixWidth()+20)))
+	svg := document.GetElementByID("map-svg")
+	svg.SetAttr("height", fmt.Sprintf("%fpx", hm.GetPixHeight()+20))
+	svg.SetAttr("width", fmt.Sprintf("%fpx", hm.GetPixWidth()+20))
 	svg.Append(mapGroup)
-	document.GetElementByID("map-contents").Append(svg)
 }
