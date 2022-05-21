5d2bdef3f2e2706a535f603b83eba5c4e2a726c6
Chris Shabsin <cshabsin@gmail.com>
2022-05-20 20:48:43 -0400

    Fill the grid with parsecs

--- example/example.go
+++ example/example.go
@@ -13,8 +13,12 @@ func main() {
 	hm := hexmap.NewHexMap(10, 11, 70, false)
 	mapGroup := document.CreateSVG("g", js.MakeAttr("class", "map-anchor-group"), js.MakeAttr("transform", "translate(10,10)"))
 	mapGroup.Append(hm.GridMesh().MakeSVG(document, js.Class("map-mesh")))
-	parsec := view.NewParsec(document, hm, 2, 2)
-	mapGroup.Append(parsec.Anchor)
+	for col := 0; col < 10; col++ {
+		for row := 0; row < 11; row++ {
+			parsec := view.NewParsec(document, hm, col, row)
+			mapGroup.Append(parsec.Anchor)
+		}
+	}
 
 	svg := document.GetElementByID("map-svg")
 	svg.SetAttr("height", fmt.Sprintf("%fpx", hm.GetPixHeight()+20))
