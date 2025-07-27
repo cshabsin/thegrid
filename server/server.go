6dcb8f0ce681d8ea66686dc5f42a1cef7a126ba6
Chris Shabsin <cshabsin@gmail.com>
2025-07-27 08:11:21 -0400

    feat: Add root handler to list available services
    This change adds a new handler for the root URL (/) that displays a list of links to the registered applications, making the server more user-friendly.
    
    Gemini commit: now that apps are registered, let's handle the / url by emitting a list of links to the available services

--- server/server.go
+++ server/server.go
@@ -3,9 +3,12 @@ package main
 import (
 	"archive/zip"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
+
+var registeredApps []string
 
 func registerApp(name, zipPath string) {
 	zipReader, err := zip.OpenReader(zipPath)
@@ -13,11 +16,35 @@ func registerApp(name, zipPath string) {
 		log.Fatalf("failed to open %s: %v", zipPath, err)
 	}
 	http.Handle(fmt.Sprintf("/%s/", name), http.StripPrefix(fmt.Sprintf("/%s/", name), http.FileServer(http.FS(zipReader))))
+	registeredApps = append(registeredApps, name)
 }
 
 func main() {
 	registerApp("solitaire", "solitaire/solitaire_pkg.zip")
 	registerApp("example", "example/example_pkg.zip")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.New("index").Parse(`
+			<html>
+				<head>
+					<title>The Grid</title>
+				</head>
+				<body>
+					<h1>Available Services</h1>
+					<ul>
+						{{range .}}
+						<li><a href="/{{.}}">{{.}}</a></li>
+						{{end}}
+					</ul>
+				</body>
+			</html>
+		`)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		t.Execute(w, registeredApps)
+	})
 
 	log.Println("Listening on :8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
