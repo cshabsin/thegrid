a398694ff2bdb7cd644cbab5b0cfbafe321191e9
Chris Shabsin <cshabsin@gmail.com>
2025-08-04 18:15:55 -0400

    Refactor delta.go to use the js library abstractions.
    Gemini commit: delta.go is set up to use the js library, but i want it to use the api in my own js library under js/

--- apps/delta/delta.go
+++ apps/delta/delta.go
@@ -4,19 +4,34 @@ import (
 	"fmt"
 
 	"github.com/cshabsin/thegrid/cardkit/card"
+	"github.com/cshabsin/thegrid/cardkit/pile"
 	"github.com/cshabsin/thegrid/cardkit/ui"
 	"github.com/cshabsin/thegrid/js"
 )
 
+type Game struct{}
+
+func (g *Game) AllCards() []*card.Card                  { return nil }
+func (g *Game) GetAllPiles() map[string]pile.Pile       { return nil }
+func (g *Game) GetPileLayout(name string) ui.PileLayout { return ui.PileLayout{} }
+func (g *Game) AddListener(func())                      {}
+func (g *Game) CheckWin() bool                          { return false }
+func (g *Game) SelectedCard() *card.Card                { return nil }
+func (g *Game) SetSelectedCard(*card.Card)              {}
+func (g *Game) MoveToFoundation(*card.Card)             {}
+func (g *Game) MoveSelectedToPile(string)               {}
+func (g *Game) FlipCard(*card.Card)                     {}
+func (g *Game) StockClicked()                           {}
+func (g *Game) ToggleDebugWin()                         {}
+func (g *Game) NewGame()                                {}
+
 func main() {
-	js.Get("document").Call("addEventListener", "DOMContentLoaded", js.FuncOf(func(this js.Value, args []js.Value) any {
+	doc := js.Document()
+	doc.AddEventListener("DOMContentLoaded", func(_ js.DOMElement, _ js.DOMEvent) {
 		fmt.Println("Hello, Delta!")
-		board := ui.NewBoard(js.Get("document").Call("getElementById", "game-board"), nil)
-		board.Draw()
-		deck := card.NewDeck(20, 20)
-		board.Add(deck)
-		return nil
-	}))
+		game := &Game{}
+		ui.NewBoard(game, doc, doc.GetElementByID("game-board"))
+	})
 
 	select {}
 }
